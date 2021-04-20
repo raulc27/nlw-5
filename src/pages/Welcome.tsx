36bfd25dd77f47e5faf6992a0c8cb2b415ee5484
Raul Castro <raulc27@gmail.com>
2021-04-20 02:50:32 -0300

    ...

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
 
 export function Welcome(){
 
     return(
         <View style={styles.container}>
-        <Text>Oi</Text>
+        <Text style={styles.title}>Gerencie suas plantas de forma fácil</Text>
 
         <Image source={wateringImg} />
 
-        <Text>
+        <Text style={styles.subtitle}>
             Não esqueça mais de regar suas plantas.
             Nós cuidamos de lembrar você sempre que precisar.
         </Text>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
             <Text>
                 >
             </Text>
@@ -33,5 +34,22 @@ const styles= StyleSheet.create({
         flex:1,
         alignItems: 'center',
         justifyContent: 'space-between'
+    },
+    title:{
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop:38
+    },
+    subtitle:{
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    button:{
+        
     }
+    
 })
