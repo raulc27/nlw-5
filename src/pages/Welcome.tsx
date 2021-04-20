183527e904b5b59f6ef99235689362356e6ecefc
Raul Castro <raulc27@gmail.com>
2021-04-20 03:21:19 -0300

    estados, interfaces etc

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
+import { Button } from '../components/Button';
 import colors from '../styles/colors';
 
 export function Welcome(){
+
+    const [visible, setVisible] = useState(false);
+
+    function handleVisibility(){
+        setVisible(true)
+    }
 
     return(
         <View style={styles.container}>
         <Text style={styles.title}>Gerencie suas plantas de forma fácil</Text>
 
-        <Image source={wateringImg} style={styles.image} />
-
+        {
+        visible && <Image source={wateringImg} style={styles.image} />
+        }
         <Text style={styles.subtitle}>
             Não esqueça mais de regar suas plantas.
             Nós cuidamos de lembrar você sempre que precisar.
         </Text>
 
-        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-            <Text style={styles.buttonText} >
-                >
-            </Text>
-        </TouchableOpacity>
+        <Button title =">" onPress={handleVisibility} />
 
         </View>
     )
