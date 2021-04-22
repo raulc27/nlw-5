96ad4f0457bf5c6de85f045666a47c270d31c003
Raul Castro <raulc27@gmail.com>
2021-04-21 22:12:56 -0300

    adicionando componentes, axios etc

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import imgUsr from '../assets/raul-github.jpeg';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Header(){
     return(
-        <View style={StyleSheet.container}>
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.userName}>Raul Castro</Text>
+            </View>
             
+            <Image source={imgUsr} style={styles.image} />
+
         </View>
     )
 }
@@ -16,6 +27,24 @@ const styles = StyleSheet.create({
         justifyContent:'space-between',
         alignItems:'center',
         paddingVertical:20,
-        backgroundColor: colors.red
+        
+        marginTop: getStatusBarHeight()
+    },
+    image:{
+        width:56,
+        height:56,
+        borderRadius: 40
+
+    },
+    greeting:{
+        fontSize:32,
+        color: colors.heading,
+        fontFamily:fonts.text
+    },
+    userName:{
+        fontSize:32,
+        fontFamily:fonts.heading,
+        color:colors.heading,
+        lineHeight:40
     }
 })
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
+import { EnvironmentButton } from '../components/EnvironmentButton';
+import { Header } from '../components/Header';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect(){
     return(
         <View style={styles.container}>
-            <Text>Selecionar Planta</Text>
+
+            <View style={styles.header} >
+            <Header/>
+
+            <Text style={styles.title} >Em qual ambiente</Text>
+            <Text style={styles.subtitle} >você quer colocar sua planta</Text>
+            </View>
+
+
+            <View>
+                <FlatList 
+                    data={[1,2,4,5]}
+                    renderItem={({item})=>(
+                        <EnvironmentButton 
+                        title="cozinha"
+                        active 
+                        />
+                    )}
+                    contentContainerStyle={styles.environmentList}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                />
+            </View>
+
+
+           
+
         </View>
     )
 }
@@ -14,8 +43,30 @@ export function PlantSelect(){
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor: colors.background,
+        backgroundColor: colors.background
+    },
+    title:{
+        fontSize:17,
+        color: colors.heading,
+        fontFamily:fonts.heading,
+        lineHeight:20,
+        marginTop:15
+    },
+    subtitle:{
+        fontFamily: fonts.text,
+        fontSize:17,
+        lineHeight:20,
+        color:colors.heading
+
+    },
+    header:{
+        paddingHorizontal:30
+    },
+    environmentList:{
+        height:40,
         justifyContent:'center',
-        alignItems:'center'
+        paddingBottom:5,
+        marginLeft:32,
+        marginVertical:32
     }
 })
